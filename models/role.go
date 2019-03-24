f48beb63e14537a11451d8abcb075f445139e51c
praetorian <filippov.a@e154.ru>
2019-03-24 23:26:40 +0700

    fix

--- models/role.go
+++ models/role.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Role struct {
-	Name		string			`json:"name"`
-	Description	string			`json:"description"`
-	Parent		*Role			`json:"parent"`
-	Children	[]*Role			`json:"children"`
-	AccessList	map[string][]string	`json:"access_list"`
-	Created_at	time.Time		`json:"created_at"`
-	Update_at	time.Time		`json:"update_at"`
+	Name        string              `json:"name"`
+	Description string              `json:"description"`
+	Parent      *Role               `json:"parent"`
+	Children    []*Role             `json:"children"`
+	AccessList  map[string][]string `json:"access_list"`
+	CreatedAt   time.Time           `json:"created_at"`
+	UpdateAt    time.Time           `json:"update_at"`
 }
